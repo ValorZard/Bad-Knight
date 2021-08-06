94afe1decf5071ab30caac0712e9c694ba6c55fe
nkcyborg <kit.heckman@gmail.com>
2021-08-05 20:21:50 -0400

    basic movement

---
+++ Player/Player.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector.normalized()
+	else:
+		velocity = Vector2.ZERO
+	move_and_collide(velocity * 1.5)
+	if Input.is_action_just_pressed("ui_accept"):
+		print("placeholder")
