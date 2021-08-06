7059384ba71f26ae8de98ce5d301736cde32f41f
Riley Park <rhpark11@gmail.com>
2021-08-06 14:32:23 -0700

    Dialogic Update
    Adds some timelines for use in the game

--- Riley/dialogicTest.gd
+++ Riley/dialogicTest.gd
@@ -8,7 +8,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var new_dialog = Dialogic.start('testTimeline')
+	var new_dialog = Dialogic.start('Parchment')
 	add_child(new_dialog)
 
 
